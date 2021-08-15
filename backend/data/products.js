d2a42d4079f9ca0ec381c48fef2dc43c39362f9b
Nguyễn Quang Trung <gnaoh17939@gmail.com>
2021-08-15 17:45:26 +0700

    aaaa

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,6 +1,6 @@
 const products = [
   {
-    name: "Airpods Wireless Bluetooth Headphones",
+    name: "Airpods Wireless Bluetooth Headphoness",
     image: "/images/airpods.jpg",
     description:
       "Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working",
